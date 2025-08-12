1384614afc80bc199d68d132d84b87da77c14237
21407alfredmunga <21407alfredmunga@gmail.com>
2025-08-12 16:56:50 +0300

    Add authentication settings and update URL patterns for login, logout, and registration views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, login_view, logout_view, register_view
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+from .views import list_books, LibraryDetailView
 from .admin_view import admin_view
 from .librarian_view import librarian_view
 from .member_view import member_view
@@ -8,9 +10,9 @@ from .book_permissions_views import add_book, edit_book, delete_book
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
-    path('register/', register_view, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
     path('admin-role/', admin_view, name='admin_view'),
     path('librarian-role/', librarian_view, name='librarian_view'),
     path('member-role/', member_view, name='member_view'),
